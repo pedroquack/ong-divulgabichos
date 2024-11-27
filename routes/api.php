f0b4f6c0baa178c560db4fb6bf95e98caac00870
pedroquack <pedrovolpattocosta@gmail.com>
2024-11-27 17:38:39 -0300

    feat: Authentication

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,25 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
 
-Route::post('/animals', [AnimalController::class,'store']);
+    Route::group(['middleware' => 'auth:sanctum'], function() {
+      Route::get('logout', [AuthController::class, 'logout']);
+      Route::get('user', [AuthController::class, 'user']);
+    });
+});
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::post('/animals', [AnimalController::class,'store']);
+    Route::delete('/animals/{id}',[AnimalController::class,'deleteAnimal']);
+    Route::put('/animals/{id}',[AnimalController::class,'updateAnimal']);
+});
+
 Route::get('/animals',[AnimalController::class,'getAllAnimals']);
 Route::get('/animals/{id}',[AnimalController::class,'getAnimal']);
-Route::delete('/animals/{id}',[AnimalController::class,'deleteAnimal']);
-Route::put('/animals/{id}',[AnimalController::class,'updateAnimal']);
+
